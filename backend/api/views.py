008df3fc5430186b0b620396ce62f989c00143b4
Yusuf Badr <yusufbadr@yahoo.com>
2025-06-23 18:37:45 +0300

    done injesting data

--- backend/api/views.py
+++ backend/api/views.py
@@ -6,13 +6,11 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 
-@api_view(["GET"])
+@api_view(["POST"])
 def api_home(request, *args, **kwargs):
-    instance = Product.objects.all().order_by("?").first()
-    data = {}
-
-    if instance:
-        # data = model_to_dict(model_data, fields=['id', 'title', 'price'])
-        data = ProductSerializer(instance).data
-
-    return Response(data)
+    serializer = ProductSerializer(data=request.data)
+    print(request.data)
+    if serializer.is_valid(raise_exception=True):
+        print(serializer.data)
+        return Response(serializer.data)
+    return Response({"invalid": "not good data"}, status=400)
--- py_client/basic.py
+++ py_client/basic.py
@@ -2,7 +2,9 @@ import requests
 
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, json={"product_id": "123"})
+get_response = requests.post(endpoint, json={"title": "Abc123", "content":"Hello World", "price": "134"})
+
+
 
 
 
