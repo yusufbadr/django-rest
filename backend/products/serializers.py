111f5607fffd869069afa5f551c32db272092531
Yusuf Badr <yusufbadr@yahoo.com>
2025-07-21 02:02:25 +0300

    serializer create and update

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -10,6 +10,7 @@ class ProductSerializer(serializers.ModelSerializer):
         view_name='product-detail',
         lookup_field='pk'
     )
+    # email = serializers.EmailField(write_only=True)
     class Meta:
         model = Product
         fields = [
@@ -22,6 +23,16 @@ class ProductSerializer(serializers.ModelSerializer):
             'sale_price',
             'my_discount',
         ]
+
+    # def create(self, validated_data):
+    #     email = validated_data.pop('email')
+    #     obj = super().create(validated_data)
+    #     print(email, obj)
+    #     return obj
+    
+    # def update(self, instance, validated_data):
+    #     email = validated_data.pop('email')
+    #     return super().update(instance, validated_data)
     
     def get_edit_url(self, obj):
         request = self.context.get('request')
